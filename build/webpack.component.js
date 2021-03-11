69d9f15c51cc93619b1c2a7a8c10ccdf7dd82bd4
轻语 <243267674@qq.com>
2021-03-11 09:39:10 +0800

    update：将每一个组件打包生成一个文件，用于按需加载。

--- build/webpack.component.js
+++ build/webpack.component.js
@@ -4,13 +4,14 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 const Components = require('./get-components')();
 
+
 const entry = {};
 Components.forEach(c => {
   entry[c] = `./packages/${c}/index.js`;
 });
 const webpackConfig = {
   mode: 'production',
-  entry: entry,
+  entry,
   output: {
     path: path.resolve(process.cwd(), './lib'),
     filename: '[name].js',
@@ -18,12 +19,26 @@ const webpackConfig = {
     libraryTarget: 'umd'
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json']
+    extensions: ['.js', '.vue', '.json'],
+    alias:  {
+      main: path.resolve(__dirname, '../src'),
+      packages: path.resolve(__dirname, '../packages'),
+      examples: path.resolve(__dirname, '../examples'),
+      'boss-lib': path.resolve(__dirname, '../')
+    },
+    modules: ['node_modules']
+  },
+  externals: {
+    vue: 'vue',
+    // 'boss-lib/packages/plugins': 'boss-lib/packages/plugins'
+  },
+  stats: 'none',
+  optimization: {
+    minimize: false
   },
   performance: {
     hints: false
   },
-  stats: 'none',  
   module: {
     rules: [{
       test: /\.js$/,
@@ -38,18 +53,19 @@ const webpackConfig = {
         }
       }
     },
+    {test: /\.scss$/, use: ["style-loader",'css-loader','sass-loader']},
     {
-        test: /\.css$/,
-        loaders: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(svg|otf|ttf|woff2?|eot|gif|png|jpe?g)(\?\S*)?$/,
-        loader: 'url-loader',
-        query: {
-          limit: 10000,
-          name: path.posix.join('static', '[name].[hash:7].[ext]')
-        }
-      }]
+      test: /\.css$/,
+      loaders: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(svg|otf|ttf|woff2?|eot|gif|png|jpe?g)(\?\S*)?$/,
+      loader: 'url-loader',
+      query: {
+        limit: 10000,
+        name: path.posix.join('static', '[name].[hash:7].[ext]')
+      }
+    }]
   },
   plugins: [
     new ProgressBarPlugin(),
