b7a247c2c9c25c0699cb2a0e140ccc4e3fe14805
sunlightt <1812327859@qq.com>
2021-03-11 23:57:52 +0800

    EditDownTextarea组件添加

---
+++ packages/EditDownTextarea/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Description: 
+ * @Author: sunlight
+ * @Date: 2021-03-11 23:24:00
+ * @LastEditTime: 2021-03-11 23:25:50
+ * @LastEditors:  
+ */
+import BsEditDownTextarea from "./src/index.vue";
+
+BsEditDownTextarea.install = function(Vue) {
+  Vue.component(BsEditDownTextarea.name, BsEditDownTextarea);
+};
+
+export default BsEditDownTextarea;
