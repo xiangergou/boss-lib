a19d191fb4c3094ccb035f759aa6ea5161e32359
sunlightt <1812327859@qq.com>
2021-03-12 15:38:26 +0800

    FilterComplex组件添加

---
+++ packages/FilterComplex/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Description: 
+ * @Author: sunlight
+ * @Date: 2021-03-12 15:15:51
+ * @LastEditTime: 2021-03-12 15:19:48
+ * @LastEditors:  
+ */
+import BsFilterComplex from "./src/index.vue";
+
+BsFilterComplex.install = function(Vue) {
+  Vue.component(BsFilterComplex.name, BsFilterComplex);
+};
+
+export default BsFilterComplex;
--- packages/index.js
+++ packages/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: 
+ * @Author: sunlight
+ * @Date: 2021-03-12 14:54:21
+ * @LastEditTime: 2021-03-12 15:39:55
+ * @LastEditors:  
+ */
 
 import vxeTable from 'boss-lib/src/plugins/vxeTable.js'
 
@@ -22,12 +29,14 @@ import BsEditInterval from './EditInterval/index.js'
 import BsTable from './Table/index.js'
 import BsLayout from './Layout/index.js'
 // import BsQuickNav from './QuickNav/index.js'  // 待处理vuex 、api
+import BsFilterComplex from './FilterComplex/index.js'
 
 const components =
   [
     BsButton, BsCardMenu, BsCrumbs, BsForm, BsJsonEditor, BsOperationLog,
     BsQuery, BsRichTextEditor, BsTab, BsTitle, BsTabSelect, BsUnitTree, BsEditDownTextarea,
-    BsUpload, BsEditDownConditions, BsTabKeepRouter, BsEditInterval, BsTable, BsLayout
+    BsUpload, BsEditDownConditions, BsTabKeepRouter, BsEditInterval, BsTable, BsLayout,
+    BsFilterComplex
   ];
 
 // 定义 install 方法
@@ -67,6 +76,7 @@ export default {
   BsTabKeepRouter,
   BsEditInterval,
   BsTable,
-  BsLayout
+  BsLayout,
+  BsFilterComplex
 };
 
